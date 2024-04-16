b849601d7903306160f63cdbfddd81ed08738a90
Mateusz Proszowski <162816124+matii002@users.noreply.github.com>
2024-04-16 18:18:40 +0200

    second commit

--- src/main/java/com/mateuszproszowski/runners/WelcomeMessage.java
+++ src/main/java/com/mateuszproszowski/runners/WelcomeMessage.java
@@ -1,7 +1,7 @@
 package com.mateuszproszowski.runners;
 
 import org.springframework.stereotype.Component;
-@Component//spirng jeśli będzie potrzebował znajdzie @Component i utworzy jego instancję
+@Component
 public class WelcomeMessage {
     public String getMessage(){
         return "Welcome to the Spring Boot!";
