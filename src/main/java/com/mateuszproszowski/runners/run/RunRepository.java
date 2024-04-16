63e6568706b0adfa49b3ca5f4a0b0bf77a9f3c04
Mateusz Proszowski <162816124+matii002@users.noreply.github.com>
2024-04-16 18:21:03 +0200

    second commit

--- src/main/java/com/mateuszproszowski/runners/run/RunRepository.java
+++ src/main/java/com/mateuszproszowski/runners/run/RunRepository.java
@@ -7,21 +7,20 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository//operacje na objektach
+@Repository
 public class RunRepository {
 
-    private List<Run> runs = new ArrayList<>();//lista przebiegnięć
+    private List<Run> runs = new ArrayList<>();
     List<Run> findAll(){
         return runs;
     }
-    //Sekwencja elementów obsługujących sekwencyjne i równoległe operacje agregujące
     Run findById(Integer id){
         return runs.stream()
                 .filter(run -> run.id() == id) //id() - getter
                 .findFirst()//zwrca opcjonalną wersje lub pusty
                 .get();
     }
-    @PostConstruct//zapełnienie bazy danych, wywołanie metody init
+    @PostConstruct
     private void init(){
         runs.add(new Run(1,"Tuesday running", LocalDateTime.now(),LocalDateTime.now().plus(30, ChronoUnit.MINUTES),5,Location.INDOOR));
         runs.add(new Run(2,"Friday running", LocalDateTime.now(),LocalDateTime.now().plus(37, ChronoUnit.MINUTES),6,Location.OUTDOOR));
